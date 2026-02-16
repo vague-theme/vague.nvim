f201638d53ac7d6e404bb3539b44fafa4917b0a1
crow-418 <73722884+crow-418@users.noreply.github.com>
2026-02-16 23:14:44 +0000

    refactor: use vim.tbl_extend() for added support
    Support all vim.api.keyset.highlight options and handle gui and fmt
    fields for compatibility.
    
    Co-authored-by: skewb1k <skewb1kunix@gmail.com>

--- lua/vague/highlights.lua
+++ lua/vague/highlights.lua
@@ -6,29 +6,23 @@ local M = {}
 local function set_vim_highlights(highlights)
   for name, setting in pairs(highlights) do
     local style_string = setting.gui or setting.fmt or ""
+    -- Clear not supported in nvim_set_hl() options
+    setting.gui = nil
+    setting.fmt = nil
 
+    style_string = style_string:gsub("none", "")
+
+    -- Parse strings like "bold,italic"
     local parsed = {}
-    -- we do this to avoid strings like ",italic" from being inserted
     for style in style_string:gmatch("([^,]+)") do
       parsed[style] = true
     end
 
-    vim.api.nvim_set_hl(0, name, {
-      fg = setting.fg,
-      bg = setting.bg,
-      sp = setting.sp,
-      bold = curr_internal_conf.bold and parsed.bold,
-      italic = curr_internal_conf.italic and parsed.italic,
-      standout = parsed.standout,
-      underline = parsed.underline,
-      undercurl = parsed.undercurl,
-      underdouble = parsed.underdouble,
-      underdotted = parsed.underdotted,
-      underdashed = parsed.underdashed,
-      strikethrough = parsed.strikethrough or setting.strikethrough,
-      reverse = parsed.reverse,
-      nocombine = parsed.nocombine,
-    })
+    parsed.bold = curr_internal_conf.bold and parsed.bold
+    parsed.italic = curr_internal_conf.italic and parsed.italic
+
+    local hl_opts = vim.tbl_extend("force", setting, parsed)
+    vim.api.nvim_set_hl(0, name, hl_opts)
   end
 end
 
