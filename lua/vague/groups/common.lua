vague-theme/vague.nvim

commit c1ab4d4891ff3a27deba6a80222d895ac8ffb2e5

fix: handle 'transparent' option for tabline and statusline

lua/vague/groups/common.lua
@@ -30,7 +30,7 @@ M.get_colors = function(conf)
     LineNr             = { fg = c.comment },
     MatchParen         = { fg = c.fg, bg = c.visual },
     MoreMsg            = { fg = c.func, gui = "bold" },
-    MsgSeparator       = { fg = c.string, bg = c.line, gui = "bold" },
+    MsgSeparator       = { fg = c.string, bg = conf.transparent and "none" or c.line, gui = "bold" },
     NonText            = { fg = c.comment },
     Normal             = { fg = c.fg, bg = conf.transparent and "none" or c.bg },
     NormalFloat        = { fg = c.fg, bg = conf.transparent and "none" or c.bg },
@@ -49,14 +49,14 @@ M.get_colors = function(conf)
     SpellCap           = { gui = "undercurl", sp = c.delta },
     SpellLocal         = { gui = "undercurl", sp = c.hint},
     SpellRare          = { gui = "undercurl", sp = c.constant },
-    StatusLine         = { fg = c.fg, bg = c.inactiveBg },
-    StatusLineTerm     = { fg = c.fg, bg = c.inactiveBg },
+    StatusLine         = { fg = c.fg, bg = conf.transparent and "none" or c.inactiveBg },
+    StatusLineTerm     = { fg = c.fg, bg = conf.transparent and "none" or c.inactiveBg },
     StatusLineNC       = { fg = c.comment },
     StatusLineTermNC   = { fg = c.comment },
     Substitute         = { fg = c.type, bg = c.visual },
-    TabLine            = { fg = c.fg, c.inactiveBg },
-    TabLineFill        = { bg = c.inactiveBg },
-    TabLineSel         = { fg = c.inactiveBg, bg = c.fg },
+    TabLine            = conf.transparent and { fg = c.comment } or { bg = c.inactiveBg },
+    TabLineSel         = conf.transparent and {} or { bg = c.inactiveBg },
+    TabLineSel         = conf.transparent and {} or { fg = c.inactiveBg, bg = c.fg },
     Visual             = { bg = c.visual },
     VisualNOS          = { bg = c.comment, gui = "underline" },
     WarningMsg         = { fg = c.warning, gui = "bold" },

commit 0e071a35a5c44b63fc447549cf7f5ec586b0798a

fix: fix incorrect highlight name
Problem: A typo was introduced by commit c1ab4d4 on line 58 which
replaced TabLineFill with TabLineSel. This causes TabLineFill to be
missing it's highlight.

Solution: Fix the highlight name so TabLineFill now has it's highlights
applied.

lua/vague/groups/common.lua
@@ -55,7 +55,7 @@ M.get_colors = function(conf)
     StatusLineTermNC   = { fg = c.comment },
     Substitute         = { fg = c.type, bg = c.visual },
     TabLine            = conf.transparent and { fg = c.comment } or { bg = c.inactiveBg },
-    TabLineSel         = conf.transparent and {} or { bg = c.inactiveBg },
+    TabLineFill        = conf.transparent and {} or { bg = c.inactiveBg },
     TabLineSel         = conf.transparent and {} or { fg = c.inactiveBg, bg = c.fg },
     Visual             = { bg = c.visual },
     VisualNOS          = { bg = c.comment, underline = true },