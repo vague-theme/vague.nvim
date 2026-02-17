53b15de1af7ce03834fe3e5b30de376cd5d186e8
crow-418 <73722884+crow-418@users.noreply.github.com>
2026-02-17 11:35:13 +0000

    style: fix comments indentation

--- lua/vague/groups/telescope.lua
+++ lua/vague/groups/telescope.lua
@@ -11,7 +11,7 @@ M.get_colors = function(conf)
     TelescopeMatching         = { fg = c.delta, gui = conf.plugins.telescope.match },  -- Matching text in Telescope
     TelescopePromptPrefix     = { fg = c.constant },                                   -- Prefix in Telescope prompt
     TelescopeSelection        = { fg = c.constant, bg = c.line },                      -- Selection in Telescope
-    TelescopeSelectionCaret   = { fg = c.constant },                                       -- Caret in Telescope selection
+    TelescopeSelectionCaret   = { fg = c.constant },                                   -- Caret in Telescope selection
     TelescopeResultsNormal    = { fg = c.fg },                                         -- Normal text in Telescope results
   }
 
