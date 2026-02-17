vague-theme/vague.nvim

commit c1ab4d4891ff3a27deba6a80222d895ac8ffb2e5

fix: handle 'transparent' option for tabline and statusline

lua/vague/groups/common.lua
@@ -30,7 +30,7 @@ M.get_colors = function(conf)
     LineNr             = { fg = c.comment },
     MatchParen         = { fg = c.fg, bg = c.visual },
     MoreMsg            = { fg = c.func, gui = "bold" },
-    MsgSeparator       = { fg = c.string, bg = c.line, gui = "bold" },
+    MsgSeparator       = { fg = c.string, bg = conf.transparent and "none" or c.line, gui = "bold" },
     NonText            = { fg = c.comment },
     Normal             = { fg = c.fg, bg = conf.transparent and "none" or c.bg },
     NormalFloat        = { fg = c.fg, bg = conf.transparent and "none" or c.bg },
@@ -49,14 +49,14 @@ M.get_colors = function(conf)
     SpellCap           = { gui = "undercurl", sp = c.delta },
     SpellLocal         = { gui = "undercurl", sp = c.hint},
     SpellRare          = { gui = "undercurl", sp = c.constant },
-    StatusLine         = { fg = c.fg, bg = c.inactiveBg },
-    StatusLineTerm     = { fg = c.fg, bg = c.inactiveBg },
+    StatusLine         = { fg = c.fg, bg = conf.transparent and "none" or c.inactiveBg },
+    StatusLineTerm     = { fg = c.fg, bg = conf.transparent and "none" or c.inactiveBg },
     StatusLineNC       = { fg = c.comment },
     StatusLineTermNC   = { fg = c.comment },
     Substitute         = { fg = c.type, bg = c.visual },
-    TabLine            = { fg = c.fg, c.inactiveBg },
-    TabLineFill        = { bg = c.inactiveBg },
-    TabLineSel         = { fg = c.inactiveBg, bg = c.fg },
+    TabLine            = conf.transparent and { fg = c.comment } or { bg = c.inactiveBg },
+    TabLineSel         = conf.transparent and {} or { bg = c.inactiveBg },
+    TabLineSel         = conf.transparent and {} or { fg = c.inactiveBg, bg = c.fg },
     Visual             = { bg = c.visual },
     VisualNOS          = { bg = c.comment, gui = "underline" },
     WarningMsg         = { fg = c.warning, gui = "bold" },

commit 24cd29d277bf5259ab347c49d223d8cfb9057565

fix: remove duplicated `TabLineSel` declaration
Typo introduced in c1ab4d4891ff3a27deba6a80222d895ac8ffb2e5.

Co-authored-by: crow-418 <73722884+crow-418@users.noreply.github.com>

lua/vague/groups/common.lua
@@ -55,7 +55,6 @@ M.get_colors = function(conf)
     StatusLineTermNC   = { fg = c.comment },
     Substitute         = { fg = c.type, bg = c.visual },
     TabLine            = conf.transparent and { fg = c.comment } or { bg = c.inactiveBg },
-    TabLineSel         = conf.transparent and {} or { bg = c.inactiveBg },
     TabLineSel         = conf.transparent and {} or { fg = c.inactiveBg, bg = c.fg },
     Visual             = { bg = c.visual },
     VisualNOS          = { bg = c.comment, gui = "underline" },