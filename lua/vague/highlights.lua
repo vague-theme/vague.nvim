e99a0380acc48665e77766d786155e7b126eb34a
crow-418 <73722884+crow-418@users.noreply.github.com>
2026-02-17 00:16:30 +0000

    refactor: switch to vim.api.nvim_set_hl() (#108)
    Use the native lua function vim.api.nvim_set_hl() instead of sticking
    with the :highlight command.
    
    With this change we're still using the old gui settings in order to keep
    compatibility till we move to the new api.
    
    Co-authored-by: skewb1k <skewb1kunix@gmail.com>

--- lua/vague/highlights.lua
+++ lua/vague/highlights.lua
@@ -2,24 +2,27 @@ local curr_internal_conf = require("vague.config.internal").current
 local groups = require("vague.groups")
 local M = {}
 
----@param highlights table <string, table>
+---@param highlights table<string, table<string, string|boolean>>
 local function set_vim_highlights(highlights)
   for name, setting in pairs(highlights) do
-    if setting.gui == "bold" and not curr_internal_conf.bold then
-      setting.gui = "none"
-    elseif setting.gui == "italic" and not curr_internal_conf.italic then
-      setting.gui = "none"
+    local style_string = setting.gui or setting.fmt or "" --[[@as string]]
+    -- Clear not supported in nvim_set_hl() options
+    setting.gui = nil
+    setting.fmt = nil
+
+    style_string = style_string:gsub("none", "")
+
+    -- Parse strings like "bold,italic"
+    local parsed = {}
+    for style in style_string:gmatch("([^,]+)") do
+      parsed[style] = true
     end
-    vim.api.nvim_command(
-      string.format(
-        "highlight %s guifg=%s guibg=%s guisp=%s gui=%s",
-        name,
-        setting.fg or "none",
-        setting.bg or "none",
-        setting.sp or "none",
-        setting.gui or "none"
-      )
-    )
+
+    parsed.bold = curr_internal_conf.bold and parsed.bold
+    parsed.italic = curr_internal_conf.italic and parsed.italic
+
+    local hl_opts = vim.tbl_extend("force", setting, parsed)
+    vim.api.nvim_set_hl(0, name, hl_opts)
   end
 end
 
