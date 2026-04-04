a53bb0cecbeb4575de7af6918c2dc98226291985
skewb1k <skewb1kunix@gmail.com>
2026-04-05 02:20:30 +0300

    refactor: link highlight groups instead of copying (#117)

--- lua/vague/groups/fzf-lua.lua
+++ lua/vague/groups/fzf-lua.lua
@@ -1,17 +1,15 @@
-local common_group = require("vague.groups.common")
 local M = {}
 
 ---@param conf VagueColorscheme.InternalConfig
 ---@return table
 M.get_colors = function(conf)
   local c = conf.colors
-  local common = common_group.get_colors(conf)
 
   -- stylua: ignore
   local hl = {
     FzfLuaHeaderBind = { fg = c.constant },
     FzfLuaHeaderText = { fg = c.parameter },
-    FzfLuaBorder     = common["FloatBorder"],
+    FzfLuaBorder     = { link = "FloatBorder" },
     FzfLuaPathLineNr = { fg = c.string },
     FzfLuaPathColNr  = { fg = c.parameter },
     FzfLuaLivePrompt = { fg = c.constant },
--- lua/vague/groups/snacks-picker.lua
+++ lua/vague/groups/snacks-picker.lua
@@ -1,31 +1,26 @@
-local telescope_group = require("vague.groups.telescope")
-local neotree_group = require("vague.groups.neotree")
 local M = {}
 
 ---@param conf VagueColorscheme.InternalConfig
 ---@return table
 M.get_colors = function(conf)
-  local telescope = telescope_group.get_colors(conf)
-  local neotree = neotree_group.get_colors(conf)
-
   -- stylua: ignore
   local hl = {
-    SnacksPickerMatch              = telescope["TelescopeMatching"],
-    SnacksPickerPrompt             = telescope["TelescopePromptPrefix"],
-    SnacksPickerBorder             = telescope["TelescopeBorder"],
-    SnacksPickerBoxBorder          = telescope["TelescopeBorder"],
-    SnacksPickerListBorder         = telescope["TelescopeBorder"],
-    SnacksPickerInputBorder        = telescope["TelescopeBorder"],
-    SnacksPickerPrewviewBorder     = telescope["TelescopeBorder"],
-    SnacksPickerCursorLine         = telescope["TelescopeSelection"],
-    SnacksPickerBoxCursorLine      = telescope["TelescopeSelection"],
-    SnacksPickerListCursorLine     = telescope["TelescopeSelection"],
-    SnacksPickerInputCursorLine    = telescope["TelescopeSelection"],
-    SnacksPickerPreviewCursorLine  = telescope["TelescopeSelection"],
-    SnacksPickerGitStatusUntracked = neotree["NeoTreeGitUntracked"],
-    SnacksPickerGitStatusAdded     = neotree["NeoTreeGitAdded"],
-    SnacksPickerGitStatusModified  = neotree["NeoTreeGitModified"],
-    SnacksPickerGitStatusStaged    = neotree["NeoTreeGitAdded"],
+    SnacksPickerMatch              = { link = "TelescopeMatching" },
+    SnacksPickerPrompt             = { link = "TelescopePromptPrefix" },
+    SnacksPickerBorder             = { link = "TelescopeBorder" },
+    SnacksPickerBoxBorder          = { link = "TelescopeBorder" },
+    SnacksPickerListBorder         = { link = "TelescopeBorder" },
+    SnacksPickerInputBorder        = { link = "TelescopeBorder" },
+    SnacksPickerPrewviewBorder     = { link = "TelescopeBorder" },
+    SnacksPickerCursorLine         = { link = "TelescopeSelection" },
+    SnacksPickerBoxCursorLine      = { link = "TelescopeSelection" },
+    SnacksPickerListCursorLine     = { link = "TelescopeSelection" },
+    SnacksPickerInputCursorLine    = { link = "TelescopeSelection" },
+    SnacksPickerPreviewCursorLine  = { link = "TelescopeSelection" },
+    SnacksPickerGitStatusUntracked = { link = "NeoTreeGitUntracked" },
+    SnacksPickerGitStatusAdded     = { link = "NeoTreeGitAdded" },
+    SnacksPickerGitStatusModified  = { link = "NeoTreeGitModified" },
+    SnacksPickerGitStatusStaged    = { link = "NeoTreeGitAdded" },
   }
 
   return hl
